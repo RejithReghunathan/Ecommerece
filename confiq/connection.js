a6311e0027846e5b6938f302d79a29ba52f3e791
Rejith Reghunathan <rejithreghunathan6378@gmail.com>
2021-02-08 20:21:33 +0530

    confiq file edit

--- confiq/connection.js
+++ confiq/connection.js
@@ -5,10 +5,10 @@ const state = {
 }
 
 module.exports.connect = function(done){
-    // const url = 'mongodb://localhost:27017'
+    const url = 'mongodb://localhost:27017'
     const dbname = 'shopping'
    
-    mongoClient.connect(process.env.MONGODB_URI,(err,data)=>{
+    mongoClient.connect(url,{ useUnifiedTopology: true },(err,data)=>{
         
         if(err) return done(err)
         state.db=data.db(dbname) 
